f96cfd2c520036775ff30a8d376a8faccfdcb18b
ShafiSQAE <shafi.sqa@gmail.com>
2025-01-08 19:53:16 +0800

    first commit

---
+++ src/SumOfTwoNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
+        int num1, num2, sum;
+
+        System.out.println("Enter the first number: ");
+        num1=scanner.nextInt();
+        System.out.println("Enter the Second number: ");
+        num2=scanner.nextInt();
+
+        sum=num1+num2;
+        System.out.println("The sum is: "+sum);
+    }
+}
